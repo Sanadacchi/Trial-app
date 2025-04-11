81a2d8abac9e5935682a85f8c6ea13d2f80c0a07
Sanadacchi <akisd47@gmail.com>
2025-04-11 10:37:10 +0100

    first commit

--- src/components/EnterButton.tsx
+++ src/components/EnterButton.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import  { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import enterSound from '../assets/enter.mp3';
 import { useNavigate } from 'react-router-dom';
